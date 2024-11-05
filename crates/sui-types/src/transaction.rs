e5974f26dcc0071f5f99e42cb3ae2832422ee392
publicqi <qi@fuzz.land>
2024-11-06 00:35:06 +0800

    add input loader cache

--- crates/sui-types/src/transaction.rs
+++ crates/sui-types/src/transaction.rs
@@ -3044,7 +3044,7 @@ impl ObjectReadResult {
 
 #[derive(Clone)]
 pub struct InputObjects {
-    objects: Vec<ObjectReadResult>,
+    pub objects: Vec<ObjectReadResult>,
 }
 
 impl std::fmt::Debug for InputObjects {
