901d5b8d795dffbf46fcb003bde6ffd3e18bc039
publicqi <qi@fuzz.land>
2024-12-11 13:40:39 -0800

    update db when reload

--- crates/sui-core/src/execution_cache/writeback_cache.rs
+++ crates/sui-core/src/execution_cache/writeback_cache.rs
@@ -1162,6 +1162,14 @@ impl WritebackCache {
     }
 
     pub fn reload_cached(&self, objects: &[ObjectID]) {
+        self.store
+            .clone()
+            .perpetual_tables
+            .objects
+            .rocksdb
+            .try_catch_up_with_primary()
+            .unwrap();
+
         for object_id in objects {
             self.cached.object_cache.invalidate(object_id);
         }
